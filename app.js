fb81abe5caa44f912e0c61d99c79edbd2a7b8bb1
Abdul qadeer <102538683+qadeer77@users.noreply.github.com>
2022-09-09 22:59:14 +0500

    Api integrate complete

---
+++ app.js
@@ -0,0 +1,18 @@
+// fetch("https://jsonplaceholder.typicode.com/todos/")
+//     .then(response => response.json())
+//     .then(data => console.log("result", data))
+//     .catch(err => console.log("error", err));
+
+
+// async function getData() {
+//     try{
+//         let data = await fetch("https://jsonplaceholder.typicode.com/todos/");
+//         let user = await data.json();
+//         console.log(user);
+//     }
+//     catch(err){
+//         console.log(`error${err}`);
+//     }
+// }
+
+// getData();
